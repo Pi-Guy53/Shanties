0720e36a95118ae0f562be24b89acf6814d0c527
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2021-01-31 11:42:27 -0500

    sea shanties v.1

---
+++ Assets/_Scripts/ship_controls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ship_controls : MonoBehaviour
+{
+    public float speed;
+    public float manuverability;
+    public GameObject cameraRotate;
+
+    private float Sail;
+    private float Tack;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //camera system
+        float h = 10 * Input.GetAxis("Mouse X");
+        cameraRotate.transform.Rotate(0, h, 0);
+
+        if (cameraRotate.transform.localRotation.y > .6f)
+        {
+            cameraRotate.transform.Rotate(0, -h, 0);
+        }
+
+        if (cameraRotate.transform.localRotation.y < -.6f)
+        {
+            cameraRotate.transform.Rotate(0, -h, 0);
+        }
+
+        Cursor.visible = false;
+
+        //Movement system
+        Sail = Input.GetAxis("Vertical") * speed ;
+        Tack = Input.GetAxis("Horizontal") * manuverability;
+
+        if(Sail < 0)
+        {
+            Sail = 0;
+        }
+
+        transform.position -= transform.forward * Sail * Time.deltaTime;
+        transform.Rotate(0, Tack * Time.deltaTime, 0);
+
+    }
+}
